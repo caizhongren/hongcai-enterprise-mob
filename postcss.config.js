5a4e2c2696679dc4a2971338d5881c6ba87ebbfa
zhangfeng <zhangfeng@hoolai.com>
2017-12-18 18:10:55 +0800

    feat(webpack)：打包配置更改

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // parser: 'postcss-less-parser',
+  plugins: {
+    'postcss-import': {},
+    'postcss-cssnext': {},
+    'cssnano': {},
+    'autoprefixer': {}
+  }
+}
--- webpack.config.dist.js
+++ webpack.config.dist.js
@@ -1,16 +1,18 @@
 var path = require('path');
 var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
 var ExtractTextPlugin = require('extract-text-webpack-plugin'); //css单独打包
 var HtmlWebpackPlugin = require('html-webpack-plugin'); //生成html
 
 //定义地址
 var ROOT_PATH = path.resolve(__dirname);
 var APP_PATH = path.resolve(ROOT_PATH, 'src'); //__dirname 中的src目录，以此类推
-var APP_FILE = path.resolve(APP_PATH, 'app'); //根目录文件app.jsx地址
+var APP_FILE = path.resolve(APP_PATH, 'App.jsx'); //根目录文件app.jsx地址
 var BUILD_PATH = path.resolve(ROOT_PATH, 'build/dist'); //发布文件所存放的目录/build/dist/前面加/报错？
 
 
 module.exports = {
+    stats: "verbose",
     entry: {
         app: APP_FILE,
         common: [
@@ -33,19 +35,18 @@ module.exports = {
         loaders: [{
             test: /\.js$/,
             exclude: /^node_modules$/,
-            loader: 'babel'
+            loader: 'babel-loader'
         }, {
             test: /\.css$/,
             exclude: /^node_modules$/,
-            loader: ExtractTextPlugin.extract('style', ['css', 'autoprefixer'])
+            loader: ExtractTextPlugin.extract(["css-loader", "postcss-loader"])
         }, {
             test: /\.less$/,
             exclude: /^node_modules$/,
-            loader: ExtractTextPlugin.extract('style', ['css', 'autoprefixer', 'less'])
-        }, {
-            test: /\.scss$/,
-            exclude: /^node_modules$/,
-            loader: ExtractTextPlugin.extract('style', ['css', 'autoprefixer', 'sass'])
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: ["css-loader", "less-loader", "postcss-loader"]
+            })
         }, {
             test: /\.(eot|woff|svg|ttf|woff2|gif|appcache)(\?|$)/,
             exclude: /^node_modules$/,
@@ -58,7 +59,7 @@ module.exports = {
         }, {
             test: /\.jsx$/,
             exclude: /^node_modules$/,
-            loaders: ['jsx', 'babel']
+            loaders: ['jsx-loader', 'babel-loader']
         }]
     },
     plugins: [
@@ -81,10 +82,10 @@ module.exports = {
         }),
         new ExtractTextPlugin('[name].css'),
         //提取出来的样式和common.js会自动添加进发布模式的html文件中，原来的html没有
-        new webpack.optimize.CommonsChunkPlugin("common", "common.bundle.js"),
+        new webpack.optimize.CommonsChunkPlugin({ name: "common", filename: "common.bundle.js" }),
         new webpack.optimize.UglifyJsPlugin({
             output: {
-                comments: false, // remove all comments
+                comments: true, // remove all comments
             },
             compress: {
                 warnings: false
@@ -92,6 +93,6 @@ module.exports = {
         })
     ],
     resolve: {
-        extensions: ['', '.js', '.jsx', '.less', '.scss', '.css'] //后缀名自动补全
+        extensions: ['.js', '.jsx', '.less', '.scss', '.css'] //后缀名自动补全
     }
 };
