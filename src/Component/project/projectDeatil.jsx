ac82db11ab22f2d96a3a9b618a1a9adc67b4cbfc
yuyang <yuyang1@hooali.com>
2017-12-20 15:00:08 +0800

    fix(jsx):删除不用的jsx文件

--- src/Component/project/projectDeatil.jsx
+++ src/Component/project/projectDeatil.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { is, fromJS} from 'immutable';
 import {Tool} from '../../Config/Tool';
 import {template, Loading} from '../common/mixin';
-import {date} from '../../filters/custom';
+import {dates} from '../../filters/custom';
 import '../../Style/projectDeatil.less';
 
 class Main extends Component {
@@ -21,7 +21,7 @@ class Main extends Component {
           repaymentDate: '',
           loanState: Number
         },
-        loading: Boolean,
+        loading: false,
       }
 
       this.getProjectDetail =(projectId) => {
@@ -114,7 +114,7 @@ class Main extends Component {
           </ul>
           <div className="creditRight repayment-plan">
           <div className="each-line">
-            <div className="column1"><span className={this.state.projects.loanState === 9 || this.state.projects.loanState === 10 ? 'ed' : ''}>预计</span>{date(this.state.projects.LoanTime)}</div>
+            <div className="column1"><span className={this.state.projects.loanState === 9 || this.state.projects.loanState === 10 ? 'ed' : ''}>预计</span>{dates(this.state.projects.LoanTime, '-')}</div>
             <div className="column2">
               <span className={`circle ${this.state.projects.loanState === 9 || this.state.projects.loanState === 10 ? 'ed' : ''}`}></span>
               <span className={`vertical-line ${this.state.projects.loanState === 9 || this.state.projects.loanState === 10 ? 'ed' : ''}`}></span>
@@ -127,7 +127,7 @@ class Main extends Component {
           { this.state.projectBills.map((item , index) => {
             return (
               <div className="each-line"  key={index}>
-                <div className="column1"><span className={item.status === 1  ? 'ed' : ''}>预计</span>{date(item.repaymentTime)}</div>
+                <div className="column1"><span className={item.status === 1  ? 'ed' : ''}>预计</span>{dates(item.repaymentTime, '-')}</div>
                 <div className="column2">
                   <span className={`circle ${item.status === 1 ? 'ed' : ''}`}></span>
                   <span className={`vertical-line ${index === this.state.projectBills.length-1 ? 'hidden' : this.state.projectBills[index+1].status === 1 ? 'ed' : ''}`}></span>
